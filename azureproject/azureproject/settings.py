1bb63bd6e3150fea0822d44b692228cf2cee3302
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2022-12-20 01:00:20 -0600

    Update settings.py

--- azureproject/azureproject/settings.py
+++ azureproject/azureproject/settings.py
@@ -128,3 +128,4 @@ WHITENOISE_USE_FINDERS=True
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
